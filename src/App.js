d383c5cc1d77794bca616d965bb9116687d0b29a
Kyle Treman <jkt290@nyu.edu>
2018-03-26 11:21:37 -0400

    Add basic scafold
    Added starter code from React docs

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Game from "./js/components/Game";
+
+export default class App extends Component {
+  render () {
+    return (<React.Fragment>
+      <Game />
+    </React.Fragment>)
+  }
+}
