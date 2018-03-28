2a03601b1db62ccdae3db33d97fedae4070cd68b
Kyle Treman <jkt290@nyu.edu>
2018-03-28 10:01:08 -0400

    Final code from tutorial

--- src/js/components/Board.js
+++ src/js/components/Board.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import Square from './Square';
 
-export default class Board extends Component {
+export default
+
+class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return (
+      <Square
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
+      />
+    );
   }
 
   render() {
-    const status = 'Next player: X';
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
